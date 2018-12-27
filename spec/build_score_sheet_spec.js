f9393ec74c0d31ef74f1350467175c6d78c77cff
WangLei <wanglei0717@outlook.com>
2018-12-27 22:48:46 +0800

    Add a spec to test the created prompt

--- spec/build_score_sheet_spec.js
+++ spec/build_score_sheet_spec.js
@@ -10,4 +10,8 @@ describe("BuildScoreSheet", () => {
     expect(buildScoreSheet.prompt).toEqual("格式：学号，学号，...");
   });
 
+  it("should have a method to print prompt", () => {    
+    const RESULT = buildScoreSheet.printPrompt();
+    expect(RESULT).toEqual("请输入要打印的学生的学号（格式：学号，学号，...），按回车提交：");
+  });
 });
