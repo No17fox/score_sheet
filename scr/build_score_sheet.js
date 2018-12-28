efd814d83db1627a992e945dc10a83e1ef130138
WangLei <wanglei0717@outlook.com>
2018-12-28 09:32:53 +0800

    Add a method for BuildScoreSheet class to verify input

--- scr/build_score_sheet.js
+++ scr/build_score_sheet.js
@@ -4,6 +4,15 @@ class BuildScoreSheet extends Build {
   constructor() {
     super("要打印的学生的学号", "格式：学号，学号，...");
   }
+
+  verifyStudentSeq(studentSeq) {
+    let studentArr = studentSeq.split(",").map(item => item = Number(item));
+    if (studentArr.reduce((acc, cur) => acc && cur)) {
+      return studentArr;
+    } else {
+      return super.promptIllegalInput();
+    }
+  }
 }
 
 module.exports = BuildScoreSheet;
--- spec/build_score_sheet_spec.js
+++ spec/build_score_sheet_spec.js
@@ -14,4 +14,16 @@ describe("BuildScoreSheet", () => {
     const RESULT = buildScoreSheet.printPrompt();
     expect(RESULT).toEqual("请输入要打印的学生的学号（格式：学号，学号，...），按回车提交：");
   });
+
+  it("should have a method to verify input", () => {
+    const INPUT = "20160101,20160102,20160103,20160104";
+    const RESULT = buildScoreSheet.verifyStudentSeq(INPUT);
+    expect(RESULT).toEqual([20160101, 20160102, 20160103, 20160104]);
+  });
+
+  it("should have a method to verify input", () => {
+    const INPUT = "20160101,20160102,张三,20160104";
+    const RESULT = buildScoreSheet.verifyStudentSeq(INPUT);
+    expect(RESULT).toEqual("请按正确的格式输入要打印的学生的学号（格式：学号，学号，...），按回车提交：");
+  });
 });
