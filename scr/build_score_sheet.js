93c093b271ea6c05e37e6f9fcb25ce3e63cb3f80
WangLei <wanglei0717@outlook.com>
2018-12-28 23:19:03 +0800

    Build students score report

--- scr/build_score_sheet.js
+++ scr/build_score_sheet.js
@@ -3,6 +3,7 @@ let Build = require("./build.js");
 class BuildScoreSheet extends Build {
   constructor() {
     super("要打印的学生的学号", "格式：学号，学号，...");
+    this.scoreReport = {};
   }
 
   verifyStudentSeq(studentSeq) {
@@ -11,6 +12,27 @@ class BuildScoreSheet extends Build {
       return studentArr;
     } else {
       return super.promptIllegalInput();
+    }
+  }
+
+  buildScoreReport(studentArr, studentDatabase) {
+    this.scoreReport.students = studentDatabase.filter(item => studentArr.includes(item.id));
+    this.scoreReport.average = this.averageScore(this.scoreReport.students);
+    this.scoreReport.median = this.medianScore(this.scoreReport.students);
+  }
+
+  averageScore(students) {
+    return students.reduce((acc, cur) => acc + cur.sum, 0) / students.length;
+  }
+
+  medianScore(students) {
+    let totalScore = students.map(item => item.sum);
+    totalScore.sort((a, b) => a - b);
+    let middle = Math.floor(totalScore.length / 2);
+    if (totalScore.length % 2 === 0) {
+      return (totalScore[middle] + totalScore[middle - 1]) / 2;
+    } else {
+      return totalScore[middle];
     }
   }
 }
--- spec/build_score_sheet_spec.js
+++ spec/build_score_sheet_spec.js
@@ -26,4 +26,63 @@ describe("BuildScoreSheet", () => {
     const RESULT = buildScoreSheet.verifyStudentSeq(INPUT);
     expect(RESULT).toEqual("请按正确的格式输入要打印的学生的学号（格式：学号，学号，...），按回车提交：");
   });
+
+  it("should have a method to build score report", () => { 
+    const STU_DATABASE = [{
+      name: "张三",
+      id: 20160101,
+      average: 82.5,
+      sum: 330,
+      "数学": 75,
+      "语文": 95,
+      "英语": 80,
+      "编程": 80,
+    }, {
+      name: "李四",
+      id: 20160102,
+      average: 81.25,
+      sum: 325,
+      "数学": 85,
+      "语文": 80,
+      "英语": 70,
+      "编程": 90,
+    }, {
+      name: "王五",
+      id: 20160103,
+      average: 81.25,
+      sum: 325,
+      "数学": 80,
+      "语文": 80,
+      "英语": 70,
+      "编程": 95,
+    }];
+
+    const INPUT = "20160101,20160102";
+    const STU_SEQ_ARR = buildScoreSheet.verifyStudentSeq(INPUT);
+    buildScoreSheet.buildScoreReport(STU_SEQ_ARR, STU_DATABASE);
+
+    expect(buildScoreSheet.scoreReport).toEqual({
+      students: [{
+        name: "张三",
+        id: 20160101,
+        average: 82.5,
+        sum: 330,
+        "数学": 75,
+        "语文": 95,
+        "英语": 80,
+        "编程": 80,
+      }, {
+        name: "李四",
+        id: 20160102,
+        average: 81.25,
+        sum: 325,
+        "数学": 85,
+        "语文": 80,
+        "英语": 70,
+        "编程": 90,
+      }],
+      average: 327.5,
+      median: 327.5
+    });
+  });
 });
